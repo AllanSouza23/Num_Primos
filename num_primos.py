f7324da30894396718d4598cd96e5afa8911b9ed
Allan Souza <allansouza23@users.noreply.github.com>
2020-10-24 23:47:14 -0300

    implementando print ao programa

--- num_primos.py
+++ num_primos.py
@@ -1,22 +1,19 @@
-# Elabore um programa em Python que recebe um número inteiro positivo qualquer de 0 a 100. O programa deverá verificar e exibir todos os números primos até o valor digitado. 
-# A verificação se um número é primo deverá ser implementada por uma função que retorna um valor lógico ("True" ou "False") ao programa (ou script) principal.
+num = int(input('Digite um numero: '))
+aux = 0
+aux1 = 0
+aux2 = num
 
-def verifica_primo(numero):
-    aux = 0
-    primos = []
-    for x in range (1, numero + 1):
-        if numero % x == 0:
-            print('\033[0;33;44m', x, end=' ')
-            aux += 1
-            primos.append(x)
-        else:
-            print(x, end=' ')
-        
-    print(f'\nO número {numero} foi divisível {aux} vezes')
-    if aux <= 2:
-        print(f'O número {numero} é primo!')
-    else: 
-        print(f'O número {numero} NÃO é primo!') 
-    print(primos)
-numero = int(input('Digite um número inteiro qualquer de 0 a 100:  '))
-verifica_primo(numero)
+while num > 1:
+    aux = (num ** (1/2))
+    aux = int(aux)
+    aux1 = 0
+    while aux >= 2:
+        if num % aux == 0:
+            aux1 += 1
+        if aux == 2:
+            if aux1 == 0:
+                print(num, end= ' ')
+        aux -= 1
+    num -=1
+if aux2 >= 3:
+    print(3, 2)   
