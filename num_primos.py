66e29ceebb2e6f57a758465544f52674f398f14d
Allan Souza <allansouza23@users.noreply.github.com>
2020-10-24 23:56:27 -0300

    implementando função pt1

--- num_primos.py
+++ num_primos.py
@@ -1,19 +1,27 @@
-num = int(input('Digite um numero: '))
-aux = 0
-aux1 = 0
-aux2 = num
-
-while num > 1:
-    aux = (num ** (1/2))
-    aux = int(aux)
+def verifica_primo(num):
+    aux = 0
     aux1 = 0
-    while aux >= 2:
-        if num % aux == 0:
-            aux1 += 1
-        if aux == 2:
-            if aux1 == 0:
-                print(num, end= ' ')
-        aux -= 1
-    num -=1
-if aux2 >= 3:
-    print(3, 2)   
+    aux2 = num
+
+    while num > 1:
+        aux = (num ** (1/2))
+        aux = int(aux)
+        aux1 = 0
+        while aux >= 2:
+            if num % aux == 0:
+                aux1 += 1
+            if aux == 2:
+                if aux1 == 0:
+                    print(num, end= ' ')
+            aux -= 1
+        num -=1
+           
+    if aux2 >= 3:
+        print(3, 2)  
+    if aux1 > 2:
+        print(f'\nO número {aux2} NÃO é primo!')
+    else:
+        print(f'\nO número {aux2} É primo!')      
+
+numero = int(input('Digite um numero: '))
+verifica_primo(numero)
