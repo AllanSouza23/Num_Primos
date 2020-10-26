f3b10cd902be33376b79cfadeda316b25396250a
Allan Souza <allansouza23@users.noreply.github.com>
2020-10-26 12:47:41 -0300

    ordenação crescente dos numeros primos

--- num_primos.py
+++ num_primos.py
@@ -1,23 +1,26 @@
 def verifica_primos(num):
     aux2 = num
-    while num > 1:
-        aux = (num ** (0.5))
-        aux = int(aux)
-        aux1 = 0
-        while aux >= 2:
-            if num % aux == 0:
-                aux1 += 1
-            if aux == 2:
-                if aux1 == 0:
-                    print(num, end= ' ')
-            aux -= 1
-        num -=1 
+    aux3=0
     if aux2 >= 3:
-        print(3, 2)  
+        print(2, 3, end=' ')  
     elif aux2 == 2:
         print(2)
     else:
         print('Não há números primos antes do valor digitado.')
+    while aux3 < num:
+        aux = (aux3 ** (0.5))
+        aux = int(aux)
+        aux1 = 0
+        
+        while aux >= 2:
+            if aux3 % aux == 0:
+                aux1 += 1
+            if aux == 2:
+                if aux1 == 0:
+                    print(aux3, end= ' ')
+            aux -= 1
+        aux3 +=1 
+    
 
 def é_primo(num):
     div = 0
@@ -40,5 +43,8 @@ if resposta == False:
     print(f'\nO número {numero} NÃO é primo!')
 else:
     print(f'\nO número {numero} É primo!')
-print(f'Os números primos até {numero}, o incluindo caso seja primo, são:')
+print(f'Os números primos até {numero} em ordem crescente, o incluindo caso seja primo, são:')
 verifica_primos(numero)
+if resposta == True and numero > 3:
+    print(numero, end=' ')
+print('\n')
